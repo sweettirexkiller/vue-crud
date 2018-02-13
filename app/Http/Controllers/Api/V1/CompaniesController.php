71152e15129db00b44a017eef9c6e927a1eaba20
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-02-13 21:36:42 +0100

    model, migration, Api controller, Api routes, -- no authentication ---, auth:make, blades for companies: next step --> vue integration

---
+++ app/Http/Controllers/Api/V1/CompaniesController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Company;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CompaniesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Company::all();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Company::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $company = Company::findOrFail($id);
+        $company->update($request->all());
+        return $company;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $company = Company::create($request->all());
+        return $company;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $company = Company::findOrFail($id);
+        $company->delete();
+        return '';
+    }
+}
