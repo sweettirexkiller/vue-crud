8e20b855a0441cb76f0cb4a099fc77c455383cc0
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-02-15 22:14:58 +0100

    works, finished. Though I still don't know vue enough

---
+++ resources/views/admin/companies/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 com-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Companies</div>
+                    <div class="panel-body table-responsive">
+                        <router-view name="companiesIndex"></router-view>
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
